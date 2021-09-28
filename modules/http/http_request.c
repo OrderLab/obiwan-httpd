c3612408566cba1dc981f734ffc7e366abb4a69e
robertking <superobertking@icloud.com>
2021-09-28 12:10:27 -0400

    Add one manual long-wait injection in http module

--- modules/http/http_request.c
+++ modules/http/http_request.c
@@ -49,6 +49,8 @@
 #include <stdarg.h>
 #endif
 
+#include <unistd.h>
+
 APLOG_USE_MODULE(http);
 
 /*****************************************************************
@@ -444,6 +446,9 @@ void ap_process_async_request(request_rec *r)
 #if APR_HAS_THREADS
     apr_thread_mutex_create(&r->invoke_mtx, APR_THREAD_MUTEX_DEFAULT, r->pool);
     apr_thread_mutex_lock(r->invoke_mtx);
+    fprintf(stderr, "orbit: http request locked, to sleep\n");
+    sleep(25);
+    fprintf(stderr, "orbit: http request slept 25s\n");
 #endif
     access_status = ap_run_quick_handler(r, 0);  /* Not a look-up request */
     if (access_status == DECLINED) {
@@ -465,11 +470,13 @@ void ap_process_async_request(request_rec *r)
             c->cs->state = CONN_STATE_SUSPENDED;
 #if APR_HAS_THREADS
         apr_thread_mutex_unlock(r->invoke_mtx);
+        fprintf(stderr, "orbit: http request unlocked 1\n");
 #endif
         return;
     }
 #if APR_HAS_THREADS
     apr_thread_mutex_unlock(r->invoke_mtx);
+    fprintf(stderr, "orbit: http request unlocked 2\n");
 #endif
 
     ap_die_r(access_status, r, HTTP_OK);
